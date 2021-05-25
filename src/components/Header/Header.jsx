2dc1c53e0303ceda97a198bb89b0f66505e4a889
Ostapiuss <ostapokolovych@gmail.com>
2021-05-25 15:21:59 +0300

    add prettier plugin and install elsing-config and finigh the header menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from 'react'
 import './Header.scss'
 import { Navigation } from '../Navigation'
 
 export function Header() {
-  return (
-    <header className="header">
-      <div className="header__logo-menu">
-        Metro <i>panel</i>
-      </div>
-    <Navigation />
-    </header>
-  );
+	return (
+		<header className='header'>
+			<div className='header__logo-menu'>
+				Metro <i>panel</i>
+			</div>
+			<Navigation />
+		</header>
+	)
 }
